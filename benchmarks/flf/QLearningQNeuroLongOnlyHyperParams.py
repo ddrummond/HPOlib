1efb3d427add976b97038b4119922e6b66a15613
Daren Drummond <darendrummond@yahoo.com>
2016-10-30 15:12:12 +0000

    Now supporting Neural Net dropout regularization.

--- benchmarks/flf/QLearningQNeuroLongOnlyHyperParams.py
+++ benchmarks/flf/QLearningQNeuroLongOnlyHyperParams.py
@@ -21,7 +21,7 @@ def myAlgo(params, **kwargs):
     reversalSig = int(params.get("reversalSig", "3"))
     #Hyper params
     #--nb_epoch=10 --layerCount=8 --firstLayerOutputCount=22 --learningRate=0.0001 --beta1=0.9 --beta2=0.999 --epsilon=1e-08
-    nb_epoch = int(params.get("nb_epoch", "35"))
+    nb_epoch = int(params.get("nb_epoch", "5"))
     layerCount = int(params.get("layerCount", "16"))
     firstLayerOutputCount = int(params.get("firstLayerOutputCount", "42"))
     learningRate = float(params.get("learningRate", "0.0672"))
@@ -32,6 +32,8 @@ def myAlgo(params, **kwargs):
     decay = float(params.get("decay", "1e-7"))
     momentum = float(params.get("momentum", "0.9"))
     nesterov = bool(params.get("nesterov", "True"))
+    inputDropOutRate = float(params.get("inputDropOutRate", "0.0"))
+    hiddenDropOutRate = float(params.get("hiddenDropOutRate", "0.0"))
 
     # **kwargs contains further information, like
     # for crossvalidation
@@ -39,7 +41,7 @@ def myAlgo(params, **kwargs):
     #    kwargs['fold'] is the current fold. The index is zero-based
     experimentHome = os.environ['EXPERIMENT_HOME'] #/home/ddrummond/PycharmProjects
     #--model=0 --trainingBatchSize=1 --layerSizeStep=1 --nb_epoch=10 --firstLayerOutputCount=22 --learningRate=0.0001 --beta1=0.9 --beta2=0.999 --epsilon=1e-08
-    cmd = r'python3 -u {:s}/tutorials/src/flf/StrategyRunner.py --random_state=1234 --writeOutputToFiles=False --inputGlobPath=tests/testData/features_reinfocement_training_*.csv --useSaveQSpace=True --trainingEpochs=15 --enterLongThresh={:.4f} --exitLongThresh={:.4f} --reversalSig={:d} --model=0 --trainingBatchSize=1 --layerSizeStep=1 --nb_epoch={:d} --layerCount={:d} --firstLayerOutputCount={:d} --learningRate={:.9f} --beta1={:.9f} --beta2={:.9f} --epsilon={:.9f} --rho={:.9f} --decay={:.9f} --momentum={:.9f} --nesterov={}'.format(experimentHome, enterLongThresh, exitLongThresh, reversalSig, nb_epoch, layerCount, firstLayerOutputCount, learningRate, beta1, beta2, epsilon, rho, decay, momentum, nesterov)
+    cmd = r'python3 -u {:s}/tutorials/src/flf/StrategyRunner.py --random_state=1234 --writeOutputToFiles=False --inputGlobPath=tests/testData/features_reinfocement_training_*.csv --useSaveQSpace=True --trainingEpochs=15 --trainingBatchSize=15 --enterLongThresh={:.4f} --exitLongThresh={:.4f} --reversalSig={:d} --model=0 --layerSizeStep=1 --nb_epoch={:d} --layerCount={:d} --firstLayerOutputCount={:d} --learningRate={:.9f} --beta1={:.9f} --beta2={:.9f} --epsilon={:.9f} --rho={:.9f} --decay={:.9f} --momentum={:.9f} --nesterov={} --inputDropOutRate={:.9f} --hiddenDropOutRate={:.9f}'.format(experimentHome, enterLongThresh, exitLongThresh, reversalSig, nb_epoch, layerCount, firstLayerOutputCount, learningRate, beta1, beta2, epsilon, rho, decay, momentum, nesterov, inputDropOutRate, hiddenDropOutRate)
     print("DD executing command: " + cmd)
     p = subprocess.Popen([cmd], shell=True, cwd=r'{:s}/tutorials/src/'.format(experimentHome), stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
     output, err = p.communicate()
