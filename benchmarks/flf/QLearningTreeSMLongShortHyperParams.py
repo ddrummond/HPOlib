5a27722afd43592456b2f6f449af984a2e025b08
Daren Drummond <darendrummond@yahoo.com>
2017-10-23 15:16:55 +0000

    Added support for: commissionRate, sigmaSAR Changed type from int to float: min_samples_leaf, min_samples_split

--- benchmarks/flf/QLearningTreeSMLongShortHyperParams.py
+++ benchmarks/flf/QLearningTreeSMLongShortHyperParams.py
@@ -26,15 +26,17 @@ def myAlgo(params, **kwargs):
     reversalSig = int(params.get("reversalSig", "3"))
 
     #Q Reward Params
-    sigma = float(params.get("sigma", "10.16294669445298"))
-    gamma = float(params.get("gamma", "0.0783743056537147"))
+    sigma = float(params.get("sigma", "3.6961"))
+    sigmaSAR = float(params.get("sigmaSAR", "3.6961"))
+    gamma = float(params.get("gamma", "2.9852"))
     beta = float(params.get("beta", "1.0"))
+    commissionRate = float(params.get("commissionRate", "0.00005"))
 
     #Hyper params
-    maxTreeDepth = int(params.get("maxTreeDepth", "181"))
-    min_samples_leaf = int(params.get("min_samples_leaf", "17"))
-    min_samples_split = int(params.get("min_samples_split", "60"))
-    n_estimators = int(params.get("n_estimators", "186"))
+    maxTreeDepth = int(params.get("maxTreeDepth", "315"))
+    min_samples_leaf = float(params.get("min_samples_leaf", "1.538641991771142E-5"))
+    min_samples_split = float(params.get("min_samples_split", "1.4606475786493775E-4"))
+    n_estimators = int(params.get("n_estimators", "207"))
     max_features = None
     if "max_features" in params:
         max_features = int(params["max_features"])
@@ -66,7 +68,7 @@ def myAlgo(params, **kwargs):
     #    kwargs['folds'] is 1 when no cv
     #    kwargs['fold'] is the current fold. The index is zero-based
     experimentHome = os.environ['EXPERIMENT_HOME'] #/home/ddrummond/PycharmProjects
-    cmd = r'python3 -u {:s}/tutorials/src/flf/StrategyRunner.py --random_state=1234 --inputGlobPath=tests/testData/features_reinfocement_training_*.csv --writeOutputToFiles=False --model=11 --trainingEpochs=15 --useSaveQSpace=True --enterLongThresh={:.4f} --exitLongThresh={:.4f} --enterShortThresh={:.4f} --exitShortThresh={:.4f} --minReverseLongThresh={:.4f} --minReverseShortThresh={:.4f} --reversalSig={:d} --maxTreeDepth={:d} --n_estimators={:d} --min_samples_leaf={:d} --min_samples_split={:d} --max_features={} --instanceOrder=0 --oob_score=False --n_jobs=-1 --warm_start=False --isSAR=False --bbTimePeriod={:d} --bbStdevs={:.2f} --rsiDCPeriodFraction={:d} --trendEMAPeriod={:d} --trendLinregPeriods={:d} --trendRateOfChangeLookBack={:d} --stdLookback={:d} --macdSlowPeriodFraction={:.2f} --macdFastPeriodFraction={:.2f} --sigma={:.4f} --gamma={:.4f} --beta={:.4f}'.format(experimentHome, enterLongThresh, exitLongThresh, enterShortThresh, exitShortThresh, minReverseLongThresh, minReverseShortThresh, reversalSig, maxTreeDepth, n_estimators, min_samples_leaf, min_samples_split, max_features, bbTimePeriod, bbStdevs, rsiDCPeriodFraction, trendEMAPeriod, trendLinregPeriods, trendRateOfChangeLookBack, stdLookback, macdSlowPeriodFraction, macdFastPeriodFraction, sigma, gamma, beta)
+    cmd = r'python3 -u {:s}/tutorials/src/flf/StrategyRunner.py --random_state=1234 --inputGlobPath=tests/testData/features_reinfocement_training_*.csv --writeOutputToFiles=False --model=11 --trainingEpochs=10 --useSaveQSpace=True --enterLongThresh={:.4f} --exitLongThresh={:.4f} --enterShortThresh={:.4f} --exitShortThresh={:.4f} --minReverseLongThresh={:.4f} --minReverseShortThresh={:.4f} --reversalSig={:d} --maxTreeDepth={:d} --n_estimators={:d} --min_samples_leaf={:.10f} --min_samples_split={:.10f} --max_features={} --instanceOrder=0 --oob_score=False --n_jobs=-1 --warm_start=False --isSAR=False --bbTimePeriod={:d} --bbStdevs={:.2f} --rsiDCPeriodFraction={:d} --trendEMAPeriod={:d} --trendLinregPeriods={:d} --trendRateOfChangeLookBack={:d} --stdLookback={:d} --macdSlowPeriodFraction={:.2f} --macdFastPeriodFraction={:.2f} --sigma={:.4f} --sigmaSAR={:.4f} --gamma={:.4f} --beta={:.4f} --commissionRate={:.5f}'.format(experimentHome, enterLongThresh, exitLongThresh, enterShortThresh, exitShortThresh, minReverseLongThresh, minReverseShortThresh, reversalSig, maxTreeDepth, n_estimators, min_samples_leaf, min_samples_split, max_features, bbTimePeriod, bbStdevs, rsiDCPeriodFraction, trendEMAPeriod, trendLinregPeriods, trendRateOfChangeLookBack, stdLookback, macdSlowPeriodFraction, macdFastPeriodFraction, sigma, sigmaSAR, gamma, beta, commissionRate)
     print("DD executing command: " + cmd)
     p = subprocess.Popen([cmd], shell=True, cwd=r'{:s}/tutorials/src/'.format(experimentHome), stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
     output, err = p.communicate()
